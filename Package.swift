6231c691852c605b24680e8ed13e09279e512947
Mohamad Hammoud <49097070+mhammoud85@users.noreply.github.com>
2026-04-28 14:28:19 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,35 +1,20 @@
+// swift-tools-version: 5.9
 import PackageDescription
 
 let package = Package(
-    name: "IDity",
-    defaultLocalization: "en",
+    name: "IDity-iOS",
     platforms: [
-        .iOS(.v15_6)
+        .iOS(.v15_6) // Set this to your framework's minimum supported version
     ],
     products: [
-        // The product points to the wrapper target
-        .library(name: "IDity", targets: ["IDity"])
+        .library(
+            name: "IDity",
+            targets: ["IDity"])
     ],
     targets: [
-        // 1. The Wrapper Target
-        // This handles your resources (PrivacyInfo, Localizable) and logic
-        .target(
-            name: "IDity",
-            dependencies: ["IDityFramework"], // Depends on the binary renamed below
-            path: "Source",
-            resources: [
-                .process("PrivacyInfo.xcprivacy"),
-                .process("Localizable.xcstrings")
-            ],
-            swiftSettings: [.enableUpcomingFeature("ExistentialAny")]
-        ),
-        
-        // 2. The Binary Target
-        // Renamed to avoid conflict with the wrapper target name
         .binaryTarget(
-            name: "IDityFramework", 
-            path: "IDity.xcframework"
+            name: "IDity",
+            path: "Frameworks/IDity.xcframework"
         )
-    ],
-    swiftLanguageModes: [.v5]
+    ]
 )
