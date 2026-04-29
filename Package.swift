94f8edd25d0c6a052eb4f2cb41467fdf1b91f98e
mohamad.hammoud@saradarbank.com <mohamad.hammoud@saradarbank.com>
2026-04-29 12:36:59 +0300

    update

--- Package.swift
+++ Package.swift
@@ -9,13 +9,21 @@ let package = Package(
     ],
     products: [
         .library(
-            name: "IDity",
-            targets: ["IDity"])
+            name: "IDity-iOS",
+            targets: ["IDity-iOS"])
     ],
     targets: [
+        // 1. The Main Target (The "Buildable" part)
+        .target(
+            name: "IDity-iOS",
+            dependencies: ["IDity"], // It depends on the binary below
+            path: "Sources/IDity-iOS"    // Points to your YourCode.swift folder
+        ),
+        
+        // 2. The Binary Target (The .xcframework)
         .binaryTarget(
             name: "IDity",
-            path: "Frameworks/IDity.xcframework",
+            path: "Frameworks/IDity.xcframework"
         )
     ]
 )
